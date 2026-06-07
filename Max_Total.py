0d5f5c6b911a5ce80b21fb704d142ddff3e59c14
chiradeep2006 <chiradeep032@gmail.com>
2026-06-07 22:08:01 +0530

    Add files via upload

---
+++ Max_Total.py
@@ -0,0 +1,15 @@
+e: 
+# Input 
+arr = list(map(int, input("Enter stock prices: 
+").split())) 
+ 
+profit = 0 
+ 
+# Process 
+for i in range(1, len(arr)): 
+ 
+    if arr[i] > arr[i - 1]: 
+        profit += arr[i] - arr[i - 1] 
+ 
+# Output 
+print("Maximum Profit:", profit)
