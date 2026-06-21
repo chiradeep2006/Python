17d581c3848256e457a0d6c6636022b3d6d725b3
chiradeep2006 <chiradeep032@gmail.com>
2026-06-21 23:00:57 +0530

    Add files via upload

---
+++ Wild_card.py
@@ -0,0 +1,20 @@
+def match(pattern, text):
+    n, m = len(pattern), len(text)
+
+    dp = [[False]*(m+1) for _ in range(n+1)]
+    dp[0][0] = True
+
+    for i in range(1, n+1):
+        if pattern[i-1] == '*':
+            dp[i][0] = dp[i-1][0]
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if pattern[i-1] == '*':
+                dp[i][j] = dp[i-1][j] or dp[i][j-1]
+            elif pattern[i-1] == '?' or pattern[i-1] == text[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+
+    return dp[n][m]
+
+print(match(input(), input()))
