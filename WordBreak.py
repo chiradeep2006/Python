affcf0ef5b2d122dd13895be35251884c7463d3c
chiradeep2006 <chiradeep032@gmail.com>
2026-07-13 19:03:39 +0530

    Add files via upload

---
+++ WordBreak.py
@@ -0,0 +1,23 @@
+def wordBreak(s, wordDict):
+    words = set(wordDict)
+    n = len(s)
+
+    dp = [False] * (n + 1)
+    dp[0] = True
+
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in words:
+                dp[i] = True
+                break
+
+    return 1 if dp[n] else 0
+
+
+# Example
+dictionary = ["i", "like", "sam", "sung", "samsung",
+              "mobile", "ice", "cream", "icecream",
+              "man", "go", "mango"]
+
+print(wordBreak("ilike", dictionary))
+print(wordBreak("ilikesamsung", dictionary))
